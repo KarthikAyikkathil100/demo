d6a508bc9e233d956cf5c7957fce762211fa4a15
Kartik Ayikkathil <kartik.ayikkathil@mindinventory.com>
2023-09-22 18:10:45 +0530

    dev

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 4000;
 // For parsing the express payloads
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-
+ 
 // CORS permission
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
